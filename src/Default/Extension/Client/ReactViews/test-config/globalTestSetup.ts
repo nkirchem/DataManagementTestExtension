6e057a4f0a240f9866ccb5d3a2c0dfb51f973e51
Nick Kirchem <nickkirc@microsoft.com>
2023-06-23 17:48:08 -0400

    Remove unneeded files

--- src/Default/Extension/Client/ReactViews/test-config/globalTestSetup.ts
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const { setupMocks } =  require("@microsoft/azureportal-reactview/test-support/setupMocks");
-
-setupMocks();
-
-export {};
